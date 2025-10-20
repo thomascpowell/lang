60fe2af8200d66783c5bb71cc66baf81455b11b6
Thomas <thomascoxpowell@gmail.com>
2025-10-20 12:21:29 -0400

    feat: add initial ast types

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -5,15 +5,59 @@ use crate::lexer::token::*;
 * */
 
 pub struct StatementList {
-    statments: Vec<Statement>
+    pub statements: Vec<Statement>,
 }
 
 pub enum Statement {
+    Assignment(Assignment),
+    Expression(Expression),
+    Return(Return),
+}
+
+pub enum Expression {
+    Literal(Token),
+    Identifier(Token),
+    Function(Function),
+    Call(Call),
+    BinaryExp(BinaryExp),
+    IfExp(IfExp),
+    ParenExp(Box<Expression>),
 }
 
 
+pub struct Assignment {
+    pub position: Position,
+    pub assignment_type: Type,
+    pub identifier: String,
+    pub expression: Expression,
+}
 
 
+pub struct Return {}
 
-// assignment
-// expression
+pub struct Function {}
+
+pub struct Call {}
+
+pub struct BinaryExp {}
+
+pub struct IfExp {}
+
+/*
+* Keywords
+* */
+
+pub enum Type {
+    I32,
+    String,
+    Bool,
+}
+
+/*
+* Metadata
+* */
+
+pub struct Position {
+    start_line: usize,
+    start_col: usize,
+}
