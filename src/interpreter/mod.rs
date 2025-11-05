de73095b6aaca2ed7c205c710ba2f0ae3a465fb8
Thomas <thomascoxpowell@gmail.com>
2025-11-05 13:49:32 -0500

    feat: add interpreter::new()

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,14 +1,32 @@
-use crate::{error_types::Error, interpreter::symbol::Scope, parser::ast::StatementList};
+use crate::{
+    error_types::Error,
+    interpreter::symbol::Scope,
+    parser::ast::{Statement, StatementList},
+};
 pub mod symbol;
 
 // TODO: interpreter
 pub fn interpret(ast: StatementList) -> Result<(), Error> {
+    let mut interpreter = Interpreter::new(ast);
     todo!()
 }
 
 struct Interpreter {
-    ast: Vec<StatementList>,
-    pos: usize,
-    scopes: Vec<Scope>,
+    pub ast: StatementList,
+    pub pos: usize,
+    pub scopes: Vec<Scope>,
 }
 
+impl Interpreter {
+    fn new(ast: StatementList) -> Self {
+        Interpreter {
+            ast: ast,
+            pos: 0,
+            scopes: Vec::new(),
+        }
+    }
+
+    fn peek(&mut self) -> Statement {
+        todo!()
+    }
+}
