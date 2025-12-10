37a514862f4beb81667ddd09e8575871666fe4fa
Thomas <thomascoxpowell@gmail.com>
2025-12-10 14:42:14 -0500

    test: remove redundant parser tests

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,5 +1,3 @@
-use std::{fs, path::Path};
-
 use lang::{
     error_types::Error,
     lexer::tokenize,
@@ -9,17 +7,6 @@ use lang::{
 #[test]
 fn parse_simple() {
     check(parse_str("i32 digit = 2;"));
-}
-
-#[test]
-fn parse_demos() {
-    let demo_dir = Path::new("demos");
-    for entry in fs::read_dir(demo_dir).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let program = fs::read_to_string(&path).unwrap();
-        let _ = parse_str(&program).map_err(|e| panic!("parse failed for {:?}: {:?}", path, e));
-    }
 }
 
 fn parse_str(program: &str) -> Result<StatementList, Error> {
