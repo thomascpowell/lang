252873ee958e49786c3de81ef529faf1b4b5b5db
Thomas <thomascoxpowell@gmail.com>
2025-12-12 00:38:41 -0500

    feat(cli): add source and lexer handlers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::env;
+use std::{env, fs::File, io::Read};
+
+use lang::lexer::tokenize;
 
 /**
 * Lang CLI
@@ -9,15 +11,48 @@ use std::env;
 const HELP: &str = include_str!("../docs/help");
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-
-    let argument = match args.pop() {
+    let args: Vec<String> = env::args().collect();
+    let argument = match args.get(1) {
         None => return print!("{}", HELP),
         Some(x) => x,
     };
 
     match argument.to_lowercase().as_str() {
         "help" => print!("{}", HELP),
-        x => print!("error: no such argument \"{}\"\n\n{}", x, HELP)
+        "source" => handle_source(get_source_from_path(args.get(2))),
+        "lexer" => handle_lexer(get_source_from_path(args.get(2))),
+        x => print!("\nerror: no such argument \"{}\"\n\n{}", x, HELP),
     }
+}
+
+/**
+* Handlers
+* */
+
+fn handle_source(source: String) {
+    println!("source:\n{}", source)
+}
+
+fn handle_lexer(source: String) {
+    match tokenize(source) {
+        Err(e) => println!("{}", e.display()),
+        Ok(t) => t.iter().for_each(|token| println!("{}", token.display())),
+    }
+}
+
+/**
+* utility functions
+* */
+
+fn get_source_from_path(path: Option<&String>) -> String {
+    get_file(path.expect("error: missing required argument <path>"))
+}
+
+fn get_file(path: &str) -> String {
+    let mut contents = Vec::new();
+    File::open(path)
+        .expect("error: invalid file path")
+        .read_to_end(&mut contents)
+        .expect("error: failed to read file");
+    String::from_utf8(contents).expect("error: invalid file contents")
 }
